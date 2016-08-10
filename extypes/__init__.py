154fc0256f0a10053fd24407c503c8d33820bea9
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2016-08-10 15:52:34 +0200

    Release extypes v1.0.1

--- extypes/__init__.py
+++ extypes/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2014 Raphaël Barrois
 # This code is distributed under the two-clause BSD License.
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Raphaël Barrois <raphael.barrois+extypes@polytechnique.org>'
 
 
